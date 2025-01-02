d727c40dd07960c7ccedfe5ea2a3de35ae0ea21f
elifesratanis <elifesratanis4@gmail.com>
2025-01-02 13:20:30 +0300

    Değişiklik açıklaması

--- backenddosyası/obj/Debug/net8.0/DepremProje.AssemblyInfo.cs
+++ backenddosyası/obj/Debug/net8.0/DepremProje.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepremProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a38987e6993c46489d883344fb5781362a8122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3ada99e17ce3c1d8ba69937347971a36e039ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepremProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepremProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
